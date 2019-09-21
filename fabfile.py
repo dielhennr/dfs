a5c3a3da77855e47b188a2f0a201f97e3cbf06af
VinDeesy <31940820+VinDeesy@users.noreply.github.com>
2019-09-20 20:18:32 -0700

    Update fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -1,3 +1,19 @@
+'''
+
+Usage:
+
+First ssh into a lab machine (doesn't matter which one). Create a virtualenv by running: 'virtualenv venv'
+Run: 'source venv/bin/activate'
+You will now be operating in a virtual env
+Install fabric with: pip install 'fabric>=1.12.0,<1.13.0'
+
+Go into the directory with fabfile.py
+Run: fab -l
+You should see 'start' as a fabric task
+Run: fab start
+Boom
+Make sure you have Controller started before you run this, and also make sure you change the path in the fabfile to the correct location of the P1-ryan folder in your home directory
+'''
 from fabric.api import task, parallel, run, execute
 
 hosts = ['orion02', 'orion03', 'orion04', 'orion05', 'orion06', 'orion07',

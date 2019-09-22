3fbde7baa3b73a96bc0690d431b81a8e1ed85365
dielhennr <rdielhenn@dons.usfca.edu>
2019-09-21 19:03:49 -0700

    adding log4j2

--- src/main/java/edu/usfca/cs/dfs/Controller.java
+++ src/main/java/edu/usfca/cs/dfs/Controller.java
@@ -3,6 +3,9 @@ package edu.usfca.cs.dfs;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import edu.usfca.cs.dfs.net.ServerMessageRouter;
 import io.netty.channel.ChannelHandlerContext;
 
@@ -10,6 +13,7 @@ public class Controller {
 
     ServerMessageRouter messageRouter;
     static ArrayList<RequestContext> storageNodes = new ArrayList<RequestContext>();
+    private static final Logger logger = LogManager.getLogger(Controller.class);
     
 
     public void start()
@@ -21,6 +25,22 @@ public class Controller {
 
     public static void main(String[] args)
     throws IOException {
+    	/** 
+    	 * Logging example -- logs will output to log/app.log in root project directory.
+    	 * Configuration currently only recognizes INFO+ so logger.trace/debug does nothing
+    	 * Use logger.info for debugging and logger.error for logging exceptions
+    	 * 
+    	 * The reason I took out Debug level is because netty uses it and logs a 
+    	 * ton of unnecessary shit
+    	 */
+    	logger.error("ERROR");
+
+        logger.trace("Entering application...");
+        logger.debug("Entering application...");
+        logger.info("Hello Log4j2...");
+        logger.error("Something is wrong with this code", new Exception("Invalid message"));
+
+        logger.trace("Exiting application...");
         Controller controller = new Controller();
         controller.start();
     }

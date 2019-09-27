c03879f85c70de508c64e29cb53dd50b6673ba23
dielhennr <rdielhenn@dons.usfca.edu>
2019-09-26 17:20:17 -0700

    remove bin

--- bin/README.md
+++
@@ -1,9 +0,0 @@
-# Project 1: DFS With Probabilistic Routing
-
-# USAGE
-
-java -cp ./target/dfs-1.0.jar edu.usfca.cs.dfs.Controller
-java -cp ./target/dfs-1.0.jar edu.usfca.cs.dfs.StorageNode
-
-See the project spec here: https://www.cs.usfca.edu/~mmalensek/cs677/assignments/project-1.html
-
--- bin/proto/build_proto.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-################################################################################
-# build_proto.sh - builds the proto files in this directory and outputs them to
-# the project source tree.
-################################################################################
-
-cd "$(cd "$(dirname "$0")" && pwd)"
-protoc ./*.proto --java_out ../src/main/java
-
